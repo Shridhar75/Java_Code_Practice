adb532d426065589367633baa1dee2febd7f40ad
Shridhar <shridharkhambe75@gmail.com>
2025-01-11 20:29:39 +0530

    Add files via upload

---
+++ String/Basic.java
@@ -0,0 +1,12 @@
+package String;
+// Java Program to demonstrate String
+public class Basic {
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using new keyword
+        String str = new String("GeeksforGeeks");
+        System.out.println(str);
+    }
+}
---
+++ String/Length.java
@@ -0,0 +1,8 @@
+package String;
+//Java program to count length of string
+public class Length {
+    public static void main(String[] args){
+        String s = "Shridhar";
+        System.out.println("Length of given string: "+s.length());
+    }
+}
